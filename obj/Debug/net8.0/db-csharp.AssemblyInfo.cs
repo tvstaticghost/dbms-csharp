249494008b93070eedd1ea33b3a7293c4c1b9c5a
Sofia DiPaolo <ray.m.arnold@gmail.com>
2024-10-13 20:52:49 -0400

    test commit

--- obj/Debug/net8.0/db-csharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/db-csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("db-csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f584f4ccf3356f6e80c50bf9108b8e6d23c7f398")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a50b45377297634206d6c706a722be852669da95")]
 [assembly: System.Reflection.AssemblyProductAttribute("db-csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("db-csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
